8d3fd0428db8dc2e700f92b38d763ad104cbeb17
liuxin <784532927@qq.com>
2018-02-13 11:05:16 +0800

    webpack基础配置

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+// console.log('app.js');
+
+// let a = 10;
+// const b = 20;
+// let c = value => value * 2;
+
+// console.log(c(20));
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('app')
+);
