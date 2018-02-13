7b86c8921e66ed9793381a7b748714f3d3b2024a
liuxin <784532927@qq.com>
2018-02-13 19:05:59 +0800

    webpack的优化配置

--- src/app.jsx
+++ src/app.jsx
@@ -8,9 +8,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css'
 import './index.css';
+import './index.scss';
 
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <div>
+    <i class="fa fa-car"></i>
+    <h1>Hello, world</h1>,
+  </div>,
   document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 module.exports = {
     entry: './src/app.jsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
+        publicPath: '/dist/',
+        filename: 'js/app.js'
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html'
+        }),
+        new ExtractTextWebpackPlugin("css/[name].css"),
+        // 提出公共模块
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'common',
+            filename: 'js/base.js'
         })
     ],
     module: {
@@ -27,12 +35,50 @@ module.exports = {
             },
             {
                 test: /\.css$/,
+                // use: [
+                //     'style-loader',
+                //     'css-loader'
+                // ]
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextWebpackPlugin.extract({
+                    fallback: "style-loader",
+                    use: ["css-loader", "sass-loader"]
+                })
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 8192,
+                            name: 'resource/[name].[ext]'
+                        }
+                    }
                 ]
             }
         ]
+    },
+    devServer: {
+        port: 9999
     }
 
 }
