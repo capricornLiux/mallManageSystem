22ca1c30adcac60f2d0e0495867171155e1b229e
liuxin <784532927@qq.com>
2018-02-14 18:32:40 +0800

    state

--- src/app.jsx
+++ src/app.jsx
@@ -23,24 +23,56 @@ import ReactDOM from 'react-dom';
 // =============================================
 import './index.scss'
 
-// 样式对象
-let style = {
-  // 属性要使用驼峰命名法
-  color: 'red',
-  fontSize: '30px'
-  // 属性值支持字符串拼接
+// // 样式对象
+// let style = {
+//   // 属性要使用驼峰命名法
+//   color: 'red',
+//   fontSize: '30px'
+//   // 属性值支持字符串拼接
+// }
+
+// let name = 'lance'
+// let flag = false;
+// let arr = ['zhangsan', 'lisi', 'wangwu']
+// let jsx = (
+//   <div className="jsx">
+//     jsx...{flag ? <p>I am {name}</p> : <p>I am not {name}</p>}
+//     {
+//       arr.map((name,index) => <p key={index}>Hello, {name}</p>)
+//     }
+//   </div>
+// );
+
+// ==============================================
+// 组件
+// function Component(){
+//   return <h1>hello world</h1>
+// }
+
+// ES6的组件
+class ES6Component extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: 'R'
+    }
+  }
+  render(){
+    // 修改state
+    setTimeout(()=>{
+      this.setState({
+        name: 'S'
+      })
+    },2000)
+    return <h2>你好,{this.state.name}</h2>
+  }
 }
 
-let name = 'lance'
-let flag = false;
-let arr = ['zhangsan', 'lisi', 'wangwu']
-let jsx = (
-  <div className="jsx">
-    jsx...{flag ? <p>I am {name}</p> : <p>I am not {name}</p>}
-    {
-      arr.map((name,index) => <p key={index}>Hello, {name}</p>)
-    }
-  </div>
-);
-
-ReactDOM.render(jsx, document.getElementById('app'));
+// ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(
+  <div>
+    {/* <Component/> */}
+    <ES6Component/>
+  </div>,
+  document.getElementById('app')
+)
