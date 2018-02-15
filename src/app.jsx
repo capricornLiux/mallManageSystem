4f372eca0188d8914b8ad0587739405b68af26d3
liuxin <784532927@qq.com>
2018-02-15 13:49:21 +0800

    react-router(2)

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 // import {HashRouter as Router, Route, Link} from "react-router-dom";
 // http://localhost:9999/dist/#/a
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 // http://localhost:9999/a 直接复制粘贴地址在新标签中访问的话,没有结果,直接请求到后端
 
 // BrowserRouter h5实现; hashRouter 哈希实现; route 路由规则; switch 路由多次匹配; Link 路由跳转;
@@ -16,7 +16,28 @@ class A extends React.Component {
   }
   render() {
     return (
-      <div>a</div>
+      <div>
+        a
+        {/* 参数: {this.props.match.params.id} */}
+        <Switch>
+
+          <Route exact path={`${this.props.match.path}`}
+          render={(route)=>{
+            return <p>不带参数的a</p>
+          }}/>
+
+          <Route path={`${this.props.match.path}/sub`}
+          render={(route)=>{
+            return <p>sub</p>
+          }}/>
+
+          <Route exact path={`${this.props.match.path}/:id`}
+          render={(route)=>{
+            return <p>带参数的a,参数是{route.match.params.id}</p>
+          }}/>
+
+        </Switch>
+      </div>
     )
   }
 }
@@ -40,7 +61,12 @@ class Wrapper extends React.Component {
     return (
       <div>
         <Link to="/a">组件a</Link>
+        <br/>
         <Link to="/b">组件b</Link>
+        <br/>
+        <Link to="/a/123">带参数的组件a</Link>
+        <br/>
+        <Link to="/a/sub">sub</Link>
         {this.props.children}
       </div>
     )
@@ -52,7 +78,11 @@ ReactDOM.render(
     <Wrapper>
       {/* <A/> */}
       {/* <B/> */}
-      <Route path='/a' component={A}/>
+      {/* <Route path='/a/:id' component={A}/> */}
+      {/* 这样没有参数的a就匹配不到了 */}
+
+      <Route path='/a' component={A}></Route>
+
       <Route path='/b' component={B}/>
     </Wrapper>
   </Router>,
